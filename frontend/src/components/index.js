22bc22d2935022b70f43cb14c3e6eeff91b5ef68
Rajuxz <rajankhanalxz@gmail.com>
2025-06-04 22:22:44 +0530

    import: routes and form

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,6 @@
 import Hero from './hero/hero';
 import Form from './forms/Form';
-export { Hero, Form };
+import LoginForm from './forms/LoginForm';
+import PublicRoute from './guards/PublicRoute';
+import ProtectedRoute from './guards/ProtectedRoute';
+export { Hero, Form, LoginForm, PublicRoute, ProtectedRoute };
