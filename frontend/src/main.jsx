024c3ce1706f6b2001cc6482e800890e4b56f145
Rajuxz <rajankhanalxz@gmail.com>
2025-06-04 22:23:20 +0530

    cleanup: removed route settings to new file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,21 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router';
-import { createBrowserRouter } from 'react-router';
+import router from './Router';
 import { ToastContainer } from 'react-toastify';
-import { Hero } from './components/index.js';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Hero />,
-    index: true,
-  },
-  {
-    path: '/dashboard',
-    element: <h1>Hello Dashboard.</h1>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
