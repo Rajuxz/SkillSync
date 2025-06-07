3de845b57c78face5208dd014a955b8ccdfbddb5
Rajuxz <rajankhanalxz@gmail.com>
2025-06-07 17:46:14 +0545

    add: sidebar

---
+++ frontend/src/components/shared/Sidebar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { RiRobot3Line } from 'react-icons/ri';
+import { SIDEBAR_ITEMS, BOTTOM_LINKS } from '../../lib/';
+import { Link } from 'react-router';
+import classNames from 'classnames';
+import { useLocation } from 'react-router';
+const linkClasses =
+  'flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-300 mb-2 rounded-md duration-400 transition-all ease-in-out';
+const Sidebar = () => {
+  return (
+    <div className="bg-white w-60 p-3 flex flex-col text-black ">
+      <div className="flex-1">
+        <div className="flex items-center gap-2 px-1 py-3">
+          <RiRobot3Line fontSize={24} />
+          <span className="text-base tracking-wide">SkillSync</span>
+        </div>
+        <div className="py-8 gap-0.5 flex flex-col flex-1 ">
+          {SIDEBAR_ITEMS.map((item) => {
+            return <SidebarLink key={item.key} item={item} />;
+          })}
+        </div>
+      </div>
+      <div className="flex flex-col gap-0.5 pt-2 border-t border-t-black">
+        {BOTTOM_LINKS.map((item) => {
+          return <SidebarLink key={item.key} item={item} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+function SidebarLink({ item }) {
+  const { pathname } = useLocation();
+  //   console.log(pathname);
+
+  return (
+    <Link
+      to={item.link}
+      className={classNames(
+        pathname === item.link ? 'text-black font-semibold' : 'text-gray-700',
+        linkClasses
+      )}
+    >
+      <span>{item.icon}</span>
+      {item.label}
+    </Link>
+  );
+}
+
+export default Sidebar;
