5b1f8af2936e75701853257c0fde4215e82bf2c4
Rajuxz <rajankhanalxz@gmail.com>
2025-06-07 17:46:48 +0545

    export: dashboard layout in single place

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,4 +3,14 @@ import Form from './forms/Form';
 import LoginForm from './forms/LoginForm';
 import PublicRoute from './guards/PublicRoute';
 import ProtectedRoute from './guards/ProtectedRoute';
-export { Hero, Form, LoginForm, PublicRoute, ProtectedRoute };
+import Dashboard from './shared/Dashboard';
+import Layout from './shared/Layout';
+export {
+  Hero,
+  Form,
+  LoginForm,
+  PublicRoute,
+  ProtectedRoute,
+  Dashboard,
+  Layout,
+};
