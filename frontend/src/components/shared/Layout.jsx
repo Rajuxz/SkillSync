a013a21caafa8aa4482cb578aaf8fc6f6c642be5
Rajuxz <rajankhanalxz@gmail.com>
2025-06-07 23:39:22 +0545

    update: state for sidebar open/close

--- frontend/src/components/shared/Layout.jsx
+++ frontend/src/components/shared/Layout.jsx
@@ -1,15 +1,23 @@
-import Sidebar from './Sidebar';
-import { Outlet } from 'react-router';
+import Sidebar from './Sidebar'
+import Header from './Header'
+import { Outlet } from 'react-router'
+import { useState } from 'react'
 const Layout = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false)
+
+  const handleToggle = () => {
+    setSidebarOpen((prev) => !prev)
+  }
+
   return (
     <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
-      <Sidebar />
-      <div className="p-4">
-        <div>Header</div>
+      <Sidebar isOpen={isSidebarOpen} />
+      <div className="flex-1">
+        <Header onToggleSidebar={handleToggle} isSidebarOpen={isSidebarOpen} />
         {<Outlet />}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+export default Layout
